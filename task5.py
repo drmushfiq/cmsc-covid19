5c3d114a3a5903f0c018fcbe8f9b3c5f1cd0e24b
Dewan Ruhul Mushfiq <drmushfiq@mun.ca>
2020-06-21 22:38:52 -0230

    created sub dataframes for easier loop iteration

--- task5.py
+++ task5.py
@@ -18,3 +18,12 @@ province=str(sys.argv[1])
 casesOrDeath=sys.argv[2]
 date=str(sys.argv[3])
 
+
+provinceDF=data.loc[data['prname'] == province] #creating a dataframe consisting only the rows of the given province
+provinceDF=provinceDF.reset_index() #resetting the index of the new dataframe
+
+rowOfGivenDate=provinceDF.index[provinceDF.date == date] #finding the row of the given date
+indexOfGivenDate=rowOfGivenDate.tolist()[0] #saving hte index number of the given date's row so that it can be iterated.
+sevenDaysDF=provinceDF.iloc[indexOfGivenDate-7:indexOfGivenDate] #creating a separate dataframe with the rows of 7days before the given date
+
+
