50737134c0acc8d9fa9a6bd89d6fd7d3c12bfa88
Dewan Ruhul Mushfiq <drmushfiq@mun.ca>
2020-06-21 22:54:52 -0230

    implemented average function to get the average case or death increase of 7days before the given date

--- task5.py
+++ task5.py
@@ -15,7 +15,7 @@ data = pd.read_csv('https://health-infobase.canada.ca/src/data/covidLive/covid19
 
 #saving given arguments into variables
 province=str(sys.argv[1])
-casesOrDeath=sys.argv[2]
+casesOrDeath=sys.argv[2].lower()
 date=str(sys.argv[3])
 
 
@@ -26,4 +26,16 @@ rowOfGivenDate=provinceDF.index[provinceDF.date == date] #finding the row of the
 indexOfGivenDate=rowOfGivenDate.tolist()[0] #saving hte index number of the given date's row so that it can be iterated.
 sevenDaysDF=provinceDF.iloc[indexOfGivenDate-7:indexOfGivenDate] #creating a separate dataframe with the rows of 7days before the given date
 
+#the function takes a dataframe as an input and returns average increase of the last 7days
+def average(DF):
+	avg=0 #setting an initial value of avg variable.
 
+	for i, row in sevenDaysDF.iterrows(): #loop to iterate over 7days of data
+		if(casesOrDeath=='cases'): #checking weather to get case rate or death rate
+			avg=avg+row['numtoday'] #sum of total case increase each day
+		else:
+			avg=avg+row['deathstoday'] #sum of total death increase each day
+	avg=avg/len(sevenDaysDF) #getting the average of the increase case/death in past 7days from the given date
+	return avg
+
+print(average(sevenDaysDF))
