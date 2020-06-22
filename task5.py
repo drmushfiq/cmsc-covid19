364569651bc2903d7e9ba10981b3663d95e6ac19
Dewan Ruhul Mushfiq <drmushfiq@mun.ca>
2020-06-21 23:19:57 -0230

    implemented function to calculate doubling days

--- task5.py
+++ task5.py
@@ -22,8 +22,8 @@ date=str(sys.argv[3])
 provinceDF=data.loc[data['prname'] == province] #creating a dataframe consisting only the rows of the given province
 provinceDF=provinceDF.reset_index() #resetting the index of the new dataframe
 
-rowOfGivenDate=provinceDF.index[provinceDF.date == date] #finding the row of the given date
-indexOfGivenDate=rowOfGivenDate.tolist()[0] #saving hte index number of the given date's row so that it can be iterated.
+indexOfGivenDate=provinceDF.index[provinceDF.date == date].tolist()[0] #saving the index number of the given date's row so that it can be iterated.
+rowOfGivenDate=provinceDF.iloc[indexOfGivenDate] #getting the row of the given date in a variable
 sevenDaysDF=provinceDF.iloc[indexOfGivenDate-7:indexOfGivenDate] #creating a separate dataframe with the rows of 7days before the given date
 
 #the function takes a dataframe as an input and returns average increase of the last 7days
@@ -38,4 +38,17 @@ def average(DF):
 	avg=avg/len(sevenDaysDF) #getting the average of the increase case/death in past 7days from the given date
 	return avg
 
-print(average(sevenDaysDF))
+#the function calculates the doubling days from the average and the current number of cases/deaths of the given date
+def predictDoublingDays(avg, currentNumber):
+	doublingDays=int(round(currentNumber/avg)) #calculating how many days it will take for the the number to double
+	if (doublingDays==0): #checking if the doubling days is 0 in which case it will say it takes 1day to double
+		doublingDays=1
+
+	return int(round(currentNumber/avg))
+
+
+avg=average(sevenDaysDF) #getting the average
+doublingDays=predictDoublingDays(avg,int(a['numdeaths'])) #getting the doubling days from the function
+
+print("avg:", avg)
+print("doubling days:", doublingDays)
